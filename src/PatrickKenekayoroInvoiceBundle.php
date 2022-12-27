cb0f39ec1f6dffa0ca6b3aae73fa6eb46a10259b
Patrick Kenekayoro <ktarila@gmail.com>
2022-12-27 20:08:15 +0100

    some update

--- src/PatrickKenekayoroInvoiceBundle.php
+++ src/PatrickKenekayoroInvoiceBundle.php
@@ -5,22 +5,30 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-
-class InvoiceBundle extends AbstractBundle
+class PatrickKenekayoroInvoiceBundle extends AbstractBundle
 {
 
      public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         // load an XML, PHP or Yaml file
-        $container->import('./Resources/config/controller_services.php');
-        $container->import('./Resources/config/routing.yml');
+        // $container->import('./Resources/config/controller_services.php');
+        $container->import('./Resources/config/services.yaml');
+        dump($this->getName());
+        // $container->import('./Resources/config/routes.yml');
 
         
     }
 
     public function getPath(): string
     {
-        return \dirname(__DIR__);
+        return __DIR__;
     }
+
+    
+
+    // public function getName(): string
+    // {
+    //     return "PatrickKenekayoroInvoiceBundle";
+    // }
 
 }
